9625327e92a9ba86daab443ebe3505b7117e1184
Duncan Marsh <dwolfm@gmail.com>
2017-07-31 13:47:00 -0700

    Never reveal your best argument.

--- src/__test__/lib/clean-db.js
+++ src/__test__/lib/clean-db.js
@@ -1,8 +1,10 @@
+import fs from 'fs-extra'
 import User from '../../model/user.js'
 import Profile from '../../model/profile.js'
 
 export default () => 
   Promise.all([
+  fs.remove(`${__dirname}/../../../temp`),
     User.remove({}),
     Profile.remove({}),
   ])
