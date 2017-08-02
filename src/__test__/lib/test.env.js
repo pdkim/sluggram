1c2a38a1d9e68f57f5f585e16553b8badca3b65e
Duncan Marsh <dwolfm@gmail.com>
2017-08-02 16:10:12 -0700

    Once I finally figured out all of life's answers, they changed the questions.

--- src/__test__/lib/test.env.js
+++ src/__test__/lib/test.env.js
@@ -1,5 +1,6 @@
 process.env.PORT = 7777
 process.env.MONGO_URI = 'mongodb://localhost/testing'
+process.env.CORS_ORIGINS = `http://localhost:${process.env.PORT}`
 process.env.SECRET = 'cool beans'
 process.env.DEBUG = true
 process.env.API_URL = `http://localhost:${process.env.PORT}`
