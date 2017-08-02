ec1d511fda03392097a8c38f934e8acc1c4fdfee
Duncan Marsh <dwolfm@gmail.com>
2017-08-02 12:13:47 -0700

    I was working on a case.  It had to be a case, because I couldn't afford a desk.  Then I saw her.  This tall blond lady.  She must have been tall because I was on the third floor.  She rolled her deep blue eyes towards me.  I picked them up and rolled them back.  We kissed.  She screamed.  I took the cigarette from my mouth and kissed her again.

--- src/__test__/lib/test.env.js
+++ src/__test__/lib/test.env.js
@@ -1,6 +1,7 @@
 process.env.PORT = 7777
 process.env.MONGO_URI = 'mongodb://localhost/testing'
 process.env.SECRET = 'cool beans'
+process.env.DEBUG = true
 process.env.API_URL = `http://localhost:${process.env.PORT}`
 process.env.AWS_ACCESS_KEY_ID='test-id'
 process.env.AWS_SECRET_ACCESS_KEY='test-secret'
