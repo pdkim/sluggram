0a76ee9ebedc3f9fbaf727d10c9467e5397e3816
Duncan Marsh <dwolfm@gmail.com>
2017-10-10 11:00:29 -0700

    added travis

--- src/__test__/lib/mock-user.js
+++ src/__test__/lib/mock-user.js
@@ -6,10 +6,10 @@ import User from '../../model/user.js'
 export const mockUser = () => {
   let result = { password: faker.internet.password() }
   return User.create({
-    username: faker.internet.userName(),
-    email: faker.internet.email(),
+    username: faker.internet.userName() + faker.internet.userName(),
+    email: faker.internet.userName() + faker.internet.email(),
     password: result.password,
-    randomHash: faker.random.uuid(),
+    randomHash: faker.random.uuid() + faker.random.uuid(),
   })
   .then(user => {
     result.user = user
