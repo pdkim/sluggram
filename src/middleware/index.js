f77c15a4035a3593efd6a56d3a778f2deb2ef79a
Phillip Kim <phildkim@gmail.com>
2018-08-26 22:28:01 -0700

    acp just in case

--- src/middleware/index.js
+++ src/middleware/index.js
@@ -17,10 +17,7 @@ import bindResponseMethods from './bind-response-methods.js'
 export default new Router()
 .use([
   // GLOBAL MIDDLEWARE
-  cors({
-    origin: process.env.CORS_ORIGINS.split(' '),
-    credentials: true,
-  }),
+  cors(),
   morgan('dev'),
   cookieParser(),
   requestReporter,
